b6c1af25c2d979c7262e9f6d5f8082192112f013
ShravyaKadur <shravya11.kadur@gmail.com>
2017-07-10 23:10:28 +0530

    Add files via upload

--- IgniteWorkspace/src/QueryMessages.java
+++ IgniteWorkspace/src/QueryMessages.java
@@ -1,7 +1,6 @@
 import org.apache.ignite.Ignite;
 import org.apache.ignite.IgniteCache;
 import org.apache.ignite.cache.query.SqlQuery;
-import org.apache.ignite.configuration.CacheConfiguration;
 import org.apache.ignite.cache.query.QueryCursor;
 import org.apache.ignite.Ignition;
 import javax.cache.Cache.Entry;
@@ -19,12 +18,18 @@ public class QueryMessages
 		// SqlQuery sql = new SqlQuery(Message.class, "select * from Message");
 		
 		// Get 10 messages of Type00
-		SqlQuery sql = new SqlQuery(Message.class, "select * from Message where msgType='Type00' limit 10");
+		//SqlQuery sql = new SqlQuery(Message.class, "select * from Message where msgType='Type00' limit 10");
+		
+		// 5 fastest messages with status 5.
+		SqlQuery sql = new SqlQuery(Message.class, "select * from Message where status=5 order by speed desc limit 5");
+		
+		//SqlQuery sql = new SqlQuery(Message.class, "select * from Message where speed=150 union select * from Message where speed=0 order by speed asc limit 10");
 		
 		try (QueryCursor<Entry<String, Message>> cursor = cache.query(sql))
 		{
+			System.out.println("MID, MsgType, Status, Speed, Time Stamp, SType, CallSign");
 			for (Entry<String, Message> e : cursor)
-			    System.out.println(e.getValue().printAll());
+			    System.out.println(e.getKey() +", "+ e.getValue().printAll());
 		}
 		
 		System.out.println("End.");
